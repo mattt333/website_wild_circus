7b13bc94ce118232b4cfc8bb242aebbd36b0c071
Matthieu Malric <mat.malric@hotmail.fr>
2020-04-18 12:57:01 +0200

    classe wilder

---
+++ Wild.java
@@ -0,0 +1,21 @@
+
+
+// Creation de la classe Wild disposant d'une méthode main
+public class Wild {
+
+	public static void main(String[] args) {
+
+     // Creation de Matthieu, un objet Wilder
+
+Wilder matthieu = new Wilder();
+      
+     // Application de la méthode sayHello de la classe Wilder
+  
+matthieu.sayHello();
+	}
+
+}
+
+
+// Creation de la classe Wilder disposant d'un prenom, nom, age, et d'une methode SayHello
+
---
+++ Wilder.java
@@ -0,0 +1,14 @@
+
+
+// Creation de la classe Wild disposant d'une méthode main
+public class Wilder {
+
+	String prenom = "Matthieu";
+	String nom = "Malric";
+	int age;
+	public void sayHello() {
+	System.out.println("Bonjour, je m'appelle "+prenom+" "+nom);
+		  }
+		
+}
+
